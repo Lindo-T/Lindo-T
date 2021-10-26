5eb57d5836ee9838ba83a174525e82d3c5817cae
Lindo-T <93193554+Lindo-T@users.noreply.github.com>
2021-10-26 12:50:27 +0200

    Update FuncVerify.py

--- FuncVerify.py
+++ FuncVerify.py
@@ -1,2 +1,18 @@
 #python3
 # Verify whether the given equation is a function
+
+"""
+A function is a relation between domain and range such that each value in the domain corresponds to only one value in the range. Relations that are not functions violate this definition. They feature at least one value in the domain that corresponds to two or more values in the range [1]
+
+Function violation is a one-to-many relation
+One input does not one output
+
+To test we must first be able to evaluate the function
+"""
+
+
+"""
+References
+[1] 3.2 Graphs of Functions and Non-Functions, Activity 4: Relations That Are Not Functions, Last Modified: Aug 19, 2019 [Online] 
+    https://flexbooks.ck12.org/cbook/ck-12-interactive-algebra-1-for-ccss/section/3.2/primary/lesson/functions-and-non-functions-alg-1-ccss/
+"""
